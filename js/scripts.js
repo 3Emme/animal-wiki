db3509d72b9fc904c76adfb6e78f9744ccf306de
3Emme <emmettbuentiempo@gmail.com>
2020-08-13 11:01:08 -0700

    add button for horse love evaluation, add conditional if function for answer to horse love evaluation.
    Co-authored-by: Cameron Will <cellardoorsolar@aol.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
-  const age = parseInt(prompt("How old are you?"));
+  const love1 = parseInt(prompt("1-10 how much do you love this horse?"));
+  const love2 = parseInt(prompt("1-10 how much do you love this horse?"));
+  const love3 = parseInt(prompt("1-10 how much do you love this horse?"));
 
-  if (age > 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Now don't go crazy!");
-    $('#drinks').show();
-  } else {
-    $('#under-21').show();
-  }
+  $("button#horse-1-button").click(function() {
+    const love1 = parseInt(prompt("1-10 how much do you love this horse?"));
+    
+    if (love1 > 5) {
+      $('#horse-1-love').show();
+    } else if (love1 === 5) {
+      alert("WRONG! Tell me how your really feel. This 'ol horse has no time for ambivalence.");
+    } else {
+      $('#horse-1-hate').show();
+    }
+  });
+
+  $("button#horse-2-button").click(function() {
+    const love2 = parseInt(prompt("1-10 how much do you love this horse?));
+    if (love2 > 5) {
+      $('#horse-2-love').show();
+    } else if (love2 === 5) {
+      alert("WRONG! Tell me how your really feel. This 'ol horse has no time for ambivalence.");
+    } else {
+      $('#horse-2-hate').show();
+    }
+  });
+
+  $("button#horse-3-button").click(function() {
+    const love3 = parseInt(prompt("1-10 how much do you love this horse?"));
+    if (love3 > 5) {
+      $('#horse-3-love').show();
+    } else if (love3 === 5) {
+      alert("WRONG! Tell me how your really feel. This 'ol horse has no time for ambivalence.");
+    } else {
+      $('#horse-3-hate').show();
+    }
+  });
+
 });
